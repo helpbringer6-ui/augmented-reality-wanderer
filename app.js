3cd4fb293a92e4e123655888e6f1fa09fdb087cc
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-10-01 10:16:56 -0500

    feat: initial commit for app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Willkommen bei der API für Augmented Reality Wanderer!');
+});
+
+app.listen(port, () => {
+    console.log(`Server läuft auf http://localhost:${port}`);
+});
