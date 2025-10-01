271ce06a948be4077cde59cb6323f5ff818d30bf
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-10-01 10:17:15 -0500

    Fix typo in German welcome message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Willkommen bei der API für Augmented Reality Wanderer!');
+    res.send('Willkommen bei der API für Augmented-Reality-Wanderer!');
 });
 
 app.listen(port, () => {
